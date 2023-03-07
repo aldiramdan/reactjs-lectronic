88ce3e77bdde9931847d78c7a6bdd38b852ccd2c
Pius <piusrestiantoro2@gmail.com>
2023-03-07 10:00:25 +0800

    login & register fixed

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./login.css"
 import {IoIosArrowBack} from "react-icons/io"
 import "bootstrap/dist/css/bootstrap.min.css"
 import image from "./img/image18.png"
 import vector from "./img/Vector.png"
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../store/reducer/user";
+import API from "../../helpers/api"
 
 function Login() {
 
@@ -13,6 +16,51 @@ function Login() {
     const handleHistory = ()=>{
         history(-1)
     }
+
+    const { isAuth } = useSelector((state) => state.users);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const api = API();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const loginHandler = async (e) => {
+      e.preventDefault();
+      try {
+        api
+          .req({
+            method: "POST",
+            url: "/auth/login",
+            data: {
+              email: email,
+              password: password,
+            },
+          })
+          .then((res) => {
+            const { token } = res.data.data;
+            dispatch(login(token));
+            navigate("/");
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    useEffect(() => {
+      if (isAuth) {
+        navigate("/");
+      }
+    }, [isAuth]);
+  
+    const inputHandler = (e) => {
+      e.preventDefault();
+  
+      if (e.target.name === "email") {
+        setEmail(e.target.value);
+      } else {
+        setPassword(e.target.value);
+      }
+    };
     
     return (
         <div className="log-container">
@@ -45,17 +93,17 @@ function Login() {
                         <p>Steps to get started, find the best stuff</p>
                         
                         <div className="log-form-group">
-                            <input type="email" name="email" placeholder="Your e-mail address" required/>
+                            <input onChange={inputHandler} type="email" name="email" placeholder="Your e-mail address" required/>
                         </div>
 
                         
                         <div className="log-form-group">
-                            <input  type="password" name="email" placeholder="Your password" required/>
+                            <input onChange={inputHandler} type="password" name="email" placeholder="Your password" required/>
                         </div>
 
                         <a className="forgot-password" href="#">Forgot Password?</a>
 
-                        <button type="submit" className="log-button-REG">Login</button>
+                        <button onClick={loginHandler} type="submit" className="log-button-REG">Login</button>
 
                         <div className="not-register">
                             <p>Not register yet? <Link className="create-acc" to={"/register"}>Create an Account</Link></p>
---
+++ src/pages/register/register.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import "./register.css"
+import {IoIosArrowBack} from "react-icons/io"
+import "bootstrap/dist/css/bootstrap.min.css"
+import image from "./img/image9.png"
+import vector from "./img/Vector.png"
+import { useNavigate } from "react-router-dom";
+import API from "../../helpers/api"
+
+function Register() {
+    
+
+    const history = useNavigate()
+
+    const handleHistory = ()=>{
+        history(-1)
+    }
+
+    const navigate = useNavigate();
+    const api = API();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const registerHandler = async (e) => {
+      e.preventDefault();
+      try {
+        api
+          .req({
+            method: "POST",
+            url: "/register",
+            data: {
+              name: name,
+              email: email,
+              password: password,
+            },
+          })
+          .then((res) => {
+            console.log(res);
+            navigate("/login");
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    const inputHandler = (e) => {
+      e.preventDefault();
+  
+      if (e.target.name === "name") {
+        setName(e.target.value);
+      } else if (e.target.name === "email") {
+        setEmail(e.target.value);
+      } else {
+        setPassword(e.target.value);
+      }
+    };
+    
+    return (
+        <div className="reg-container">
+            
+            <div>
+                <div className="right-bg col-md-5">
+                    
+                    <img src={image} alt="pic" />
+
+                    <div className="box">
+
+                        <div className="message-box row">
+                            <h3 id="h3">Welcom to Lectronic</h3>
+                            <p>We are an e-commerce that is engaged in buying and<br/>selling electronic goods, get our special offer now!</p>
+                        </div>
+                        
+                        <img className="vector" src={vector} alt="vector1" />
+                    </div>
+
+                </div>
+                
+                <div className="form-container">
+
+                <button className="arrow-back" onClick={handleHistory}><IoIosArrowBack/></button>
+
+                    <form className="register-form">
+                        
+                        <h1 className="text-left mb-5">Welcome, Please<br/>Create an Account</h1>
+
+                        <p>Please fill in your name, email, and password</p>
+
+                        <div className="reg-form-group">
+                            <input onChange={inputHandler} type="text" name="name" placeholder="What's your name?" required/>
+                        </div>
+
+                        
+                        <div className="reg-form-group">
+                            <input onChange={inputHandler} type="email" name="email" placeholder="Your e-mail address" required/>
+                        </div>
+
+                        
+                        <div className="reg-form-group">
+                            <input onChange={inputHandler} type="password" name="email" placeholder="Your password" required/>
+                        </div>
+
+                        <button type="submit" className="reg-button-REG" onClick={registerHandler}>Register</button>
+
+                    </form>
+                </div>
+            
+            </div>
+
+        </div>
+    )
+}
+
+export default Register
