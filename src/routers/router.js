3662757d27be67c84b3dbef15b54ca7cf8f8c2e0
Pius <piusrestiantoro2@gmail.com>
2023-03-07 11:46:02 +0800

    router fixing

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
 import Product from "../pages/product/product";
-import Register from "../pages/signup/register";
+import Register from "../pages/register/register";
 import Login from "../pages/login/login";
-import Profile from "../pages/profile";
 import Cart from "../pages/cart/cart";
 import DetailProduct from "../pages/product/productDetail";
 import Checkout from "../pages/checkout/checkout";
 import History from "../pages/history";
-import Admin from "../pages/admin";
+import Profile from "../pages/profile/profile";
 
 function Routers() {
 	return (
@@ -17,7 +16,6 @@ function Routers() {
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/products" element={<Product />} />
-				<Route exact path="/admin" element={<Admin />} />
 				<Route exact path="/products/detail/:id" element={<DetailProduct />} />
 				<Route exact path="/register" element={<Register />} />
 				<Route exact path="/login" element={<Login />} />
