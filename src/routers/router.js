217a750271e7e86a28f99ab85e8d285c93634c36
aldiramdan <aldi.ramdan96@gmail.com>
2023-03-07 07:35:02 +0700

    router

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,22 +1,33 @@
-import { React } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../pages/home/index'
-import Login from '../pages/login/login'
-import Product from '../pages/product/product'
-import Register from '../pages/signup/register'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/home";
+import Product from "../pages/product";
+import Register from "../pages/register";
+import Login from "../pages/login";
+import Profile from "../pages/profile";
+import Cart from "../pages/cart/cart";
+import DetailProduct from "../pages/product/detail";
+import Checkout from "../pages/checkout/checkout";
+import History from "../pages/history";
+import Admin from "../pages/admin";
 
-function router() {
+function Routers() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/products" element={<Product />} />
-				
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/products" element={<Product />} />
+				<Route exact path="/admin" element={<Admin />} />
+				<Route exact path="/products/detail/:id" element={<DetailProduct />} />
+				<Route exact path="/register" element={<Register />} />
+				<Route exact path="/login" element={<Login />} />
+				<Route exact path="/profile" element={<Profile />} />
+				<Route exact path="/cart/:id" element={<Cart />} />
+				<Route exact path="/checkout/:id" element={<Checkout />} />
+				<Route exact path="/history" element={<History />} />
 			</Routes>
 		</BrowserRouter>
-	)
+	);
 }
 
-export default router
+export default Routers;
